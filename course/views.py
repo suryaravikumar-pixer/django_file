8f9d2f4ef205bc3a5267bdfc55c935c86d5c72d5
surya <suryaravikumar5@gmail.com>
2020-08-05 23:23:42 +0530

    we added few urls

--- course/views.py
+++ course/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def learn_django(request):
+    return HttpResponse('<h1>Hello django</h1>')
+
+def learn_python(request):
+    return HttpResponse('<h1>hello python</h1>')
